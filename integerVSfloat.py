3f34dd58a51829f1013f62f6fa9ba0c4f1f21c5f
theDTS1 <orel1one@gmail.com>
2022-06-01 20:35:43 +0300

    first couple of lectures completed

---
+++ integerVSfloat.py
@@ -0,0 +1,22 @@
+print(type(6.4))
+
+# 1 Integers and Floats
+# 1.1. Integers Whole Numbers only
+#Positive or negative
+# No decimal point
+# 1.2. Float - with a decimal point
+
+# type(6.4)
+
+# Floats have a level of prec.
+
+"""
+number notation - we can use underscore to notate 
+Ints cannot start with 0 (zero) ! 
+
+"""
+# If we do int+float the return is awlays a float!
+
+# we can use ** to do a x^(X)
+
+# integer devision - we do it with // it rounds down!
---
+++ variables_in_python.py
@@ -0,0 +1,16 @@
+"""
+
+valid names:
+variable123
+variable_name - snake casing
+we can use cammelCasing too! 
+
+"""
+
+
+# help("keywords") - will list all "reserved" words for a varible.
+
+prize_money = 500
+prize_money = prize_money + 50
+print(prize_money)
+# we can also do - prize_money +=50 it's the same thing.
