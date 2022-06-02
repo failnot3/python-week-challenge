a6c591d97902982e8df65b4c53dd1f24dfd3222a
theDTS1 <orel1one@gmail.com>
2022-06-02 22:49:40 +0300

    strings

--- strings.py
+++ strings.py
@@ -3,3 +3,8 @@ url = "google.com "
 print("Visit " + url * 2)
 # Index of a string
 print(url[0])
+
+# Slice
+print(url[0:6])
+# Every second letter of the string [start:end:step]
+print(url[0:6:2])
