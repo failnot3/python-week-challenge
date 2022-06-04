958a804275d0bc7c86538cab61fe202e95d2955e
theDTS1 <orel1one@gmail.com>
2022-06-04 23:44:04 +0300

    update to methods

--- strings_and_methods.py
+++ strings_and_methods.py
@@ -7,3 +7,12 @@ name.capitalize()
 # for HELP page - name.capitalize?
 # another example
 name.lower()
+
+# .strip() return a copy of our string by default it removes spaces, new line etc.
+" hello \t Mike".strip()
+# If we provide a argument to it, it can remove a char we type.
+"....Hello ....".strip(".")
+
+# another method is replace(old, new, [count]) OLD and NEW is req. arguments!
+races = " 3kilometers 5kilometers"
+races.replace("kilometers", "km")
