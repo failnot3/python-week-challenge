e47ba2d45524d1d6044fbbcc06c5d12ab647aae1
theDTS1 <orel1one@gmail.com>
2022-06-04 23:37:01 +0300

    update

--- strings_and_methods.py
+++ strings_and_methods.py
@@ -1,2 +1,9 @@
 # Methods are just functions.
 # METHODS automatically have access to the object they are called on!
+
+# EXAMPLE: thing.method()
+name = "Petar"
+name.capitalize()
+# for HELP page - name.capitalize?
+# another example
+name.lower()
