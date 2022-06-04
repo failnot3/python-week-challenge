3c920f9c0b67e8ad64867027e32cb6206b2302f3
theDTS1 <orel1one@gmail.com>
2022-06-04 23:10:10 +0300

    Update

--- strings_and_built-ins.py
+++ strings_and_built-ins.py
@@ -1,3 +1,8 @@
 # Functions - are like a little machine that do something
 # Functions are reusable actions that have a name
 # To execute a function we use random_function()
+
+# print() is a built in function!
+# help("int") is an another built in function!
+
+# Arguments are inputs we provide to a function.
